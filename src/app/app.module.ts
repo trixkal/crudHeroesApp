e3e20f26b9da716f1be54b4d656b25818d42b95e
unknown <trixkal@gmail.com>
2020-03-11 12:55:03 -0600

    roc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './pages/hero/hero.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
